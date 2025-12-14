aa005f1adbd9e8c0f4de12bb8f627bbb72645b84
sharlin <pate2551@pnw.edu>
2025-12-14 14:35:55 -0600

    Update parse.c

--- src/parse.c
+++ src/parse.c
@@ -14,7 +14,7 @@ char **parse_input(char *input) {
     char *token = strtok(input, " \t\n");
 
     while (token != NULL && i < MAX_ARGS - 1) {
-        argv[i] = strdup(token);
+        argv[i] = strdup(token); 
         i++;
         token = strtok(NULL, " \t\n");
     }
@@ -25,12 +25,10 @@ char **parse_input(char *input) {
 
 void free_argv(char **argv) {
     if (!argv) return;
-
     for (int i = 0; i < MAX_ARGS; i++) {
         if (argv[i] != NULL) {
             free(argv[i]);
         }
     }
-    
     free(argv);
 }
